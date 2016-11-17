89d5825a39c0858bfb5130de8706b415f622aaf4
Tejaswi Vemulapati (Class of 2020) <tejaswivemulapati@527-imac26.admin.sfhs.com>
2016-11-17 15:48:27 -0800

    Init commit

--- PDA/ViewController.swift
+++ PDA/ViewController.swift
@@ -1,25 +1,72 @@
-//
-//  ViewController.swift
-//  PDA
-//
-//  Created by Tejaswi Vemulapati (Class of 2020) on 11/17/16.
-//  Copyright © 2016 Tejaswi Vemulapati (Class of 2020). All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    var ArrayOfImages = ["Scared.jpg", "Anxious.jpg", "Sad.jpg", "Angry.jpg", "Surprised.jpg", "Happy.jpg"]
+    var ArrayOfAnswers = ["Scared", "Anxious", "Sad", "Angry", "Surprised", "Happy"]
+    var RightAnswer:String!
+    var ImagesAreDone = [false, false, false, false, false, false]
+    var ImagesDone:Bool!
+    var QuestionNum:Int!
+    var Score:Int!
+    @IBOutlet var Image:UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ImagesDone = false
+        QuestionNum = 0
+        Score = 0
     }
-
+    func ChangeQuestion(){
+        if(QuestionNum >= 5){
+            print("fhajfkl s")
+        }
+        else{
+            QuestionNum = QuestionNum + 1
+            print(String(QuestionNum))
+            GetNewImage()
+        }
+    }
+    @IBAction func OptionClicked(sender:UIButton){
+        
+        ChangeQuestion()
+        CheckAnswer(buttonTitle: String(sender.titleLabel!.text!))
+        print(String(sender.titleLabel!.text!))
+    }
+    func GetNewImage(){
+        let newIndex = Int(arc4random_uniform(5))
+        if(ImagesAreDone[newIndex] == true){
+            GetNewImage()
+        }
+        else{
+            Image.image = UIImage(named:ArrayOfImages[newIndex])
+            ImagesAreDone[newIndex] = true
+            print(String(newIndex))
+            RightAnswer = ArrayOfAnswers[newIndex]
+        }
+    }
+    func CheckAnswer(buttonTitle:String){
+        if(String(buttonTitle) == RightAnswer){
+            print("YAYAYYAY")
+            UpdateScore(right: true)
+        }
+        else{
+            print("YU STUPIDDDDD")
+            UpdateScore(right: false)
+        }
+    }
+    func UpdateScore(right:Bool){
+        if(right == true){
+            Score = Score + 1
+            print(String(Score))
+        }
+        else{
+            print("h jfshdljkadshf aslfkasfga")
+            print(String(Score))
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    
 }
-
